20309a455bc1dfcab35e50bdc8d7686fe63a10f7
wilson0x4d <mrshaunwilson@msn.com>
2025-05-08 15:24:28 -0500

    docs: add missing docstrings for `OpenApiConfiguration`

--- docs/ref/OpenApiConfiguration.rst
+++ docs/ref/OpenApiConfiguration.rst
@@ -4,4 +4,16 @@ OpenApiConfiguration
 .. currentmodule:: tornado_openapi
 
 .. autoclass:: OpenApiConfiguration
-   :members:
+    :members:
+
+    .. autoattribute:: disableSchemaNamespaces
+
+    .. autoattribute:: filter
+
+    .. autoattribute:: info
+
+    .. autoattribute:: pattern
+
+    .. autoattribute:: securitySchemes
+
+    .. autoattribute:: staticFilesPath
--- tornado_openapi/OpenApiConfiguration.py
+++ tornado_openapi/OpenApiConfiguration.py
@@ -12,8 +12,14 @@ class OpenApiConfiguration:
     The configuration to be used when generating an OAS document.
     """
     disableSchemaNamespaces:bool
+    """An override option to disable schema namespacing. Can result in collisions, should be used with caution. Default is ``False``."""
     filter:Callable[[str], bool]
+    """A callback/predicate function to filter ``tag`` content. Useful for separating OAS by API Version, or similar, where you want to have two OAS endpoints, two configurations, and then need to filter which tags/APIs appear in each OAS. Default is ``lambda e: True``."""
     info:Info
+    """The Info Object to be used when describing the API. Default is ``None``."""
     pattern:str
+    """The path match pattern to be used with Tornado for serving OAS documents and (when installed) ``swagger-ui``. Default is ``r'/(swagger.*)'``."""
     securitySchemes:dict[str,SecurityScheme]
+    """The Security Schema Objects defined for the the API. Default is ``None``."""
     staticFilesPath:str
+    """The static files path where ``swagger-ui`` can be found. Default is ``./swagger-ui``."""
